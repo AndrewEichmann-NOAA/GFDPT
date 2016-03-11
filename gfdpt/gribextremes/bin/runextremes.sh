9af91f50696ffeed32ae3daa636532ae8c9fc4dc
andrew.eichmann@noaa.gov <andrew.eichmann@noaa.gov>
2016-03-11 19:19:00 +0000

    included path to executable

--- gfdpt/gribextremes/bin/runextremes.sh
+++ gfdpt/gribextremes/bin/runextremes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #export execfile=/data/users/kkumar/GFDPT-Project/fcstdiv/sorc/gribextremes.gfs.ecmwf.fd/gribextrm
-export execfile=./gribextrm
+export execfile=../src/gribextrm
 export input_ges=pgbf06.gfs.2013070118
 export input_gfs=pgbanl.gfs.2013070200
 export input_ecmwf=pgbanl.ecm.2013070200
