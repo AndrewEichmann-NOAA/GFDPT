90b3530fe2643c3cffa8bd1b62fcd36d53fb68d7
andrew.eichmann@noaa.gov <andrew.eichmann@noaa.gov>
2016-06-22 19:33:18 +0000

    AFE: before attempting to feed all the points to plottarget

--- gfdpt/plotrad/makeradplots.py
+++ gfdpt/plotrad/makeradplots.py
@@ -40,6 +40,7 @@ def plotglobal(m,obs,obsxpt,obsypt,titlestr,vmin,vmax,targetvars,targetamps,targ
 	textypt=targetypt[target]+yoffset
 	textxpts.append(textxpt)
 	textypts.append(textypt)
+        
 	plt.text(textxpt,textypt,targetstr)
 
 #    cbar = m.colorbar(foo,location='bottom',pad="5%")
@@ -48,7 +49,6 @@ def plotglobal(m,obs,obsxpt,obsypt,titlestr,vmin,vmax,targetvars,targetamps,targ
     cbar = m.colorbar(scalarMap,location='bottom',pad="5%")
     cbar.set_label('brightness temperature (K)')
 
-#    plt.title('Tb ' + date + ' ' + instrument + ' Ch ' + str(ichan),y=1.06)
     plt.title(titlestr,y=1.06)
 
 
@@ -81,17 +81,11 @@ def plottarget(obs,obslats,obslons,maxlat,minlat,maxlon,minlon,titlestr,vmin,vma
     maplats=np.arange(minlat,maxlat)
     maplons=np.arange(minlon,maxlon)
   
-#    print maplats,maplons
-
-
     parallellats=maplats[maplats%5==0]
     if abs(targetlat) > 50 :
         meridianlons=maplons[maplons%10==0]
     else:
         meridianlons=maplons[maplons%5==0]
-
-#    print parallellats,meridianlons
-
 
 
     m.drawcoastlines()
@@ -99,7 +93,6 @@ def plottarget(obs,obslats,obslons,maxlat,minlat,maxlon,minlon,titlestr,vmin,vma
     m.drawmeridians(meridianlons,labels=[True,True,True,True])
     m.drawparallels(parallellats,labels=[True,True,True,True]) 
 
-    print targetlat, targetlon, vmin,vmax    
 #    if obs.size > 0 :
 #    cNorm  = colors.Normalize(vmin=min(obs), vmax=max(obs))
 
@@ -114,8 +107,6 @@ def plottarget(obs,obslats,obslons,maxlat,minlat,maxlon,minlon,titlestr,vmin,vma
 #    foo=m.scatter(obsxpt,obsypt,c=obs,cmap=cm,marker='.',s=300,edgecolors='black',linewidth=0.15) 
 #    foo=m.scatter(obsxpt,obsypt,c=obs,cmap=cm,marker='.',s=300,edgecolors='black',linewidth=0.15) 
     foo=m.scatter(obsxpt,obsypt,c=colorVal,marker='.',s=300,edgecolors='black',linewidth=0.15) 
-
-    print foo.get_clim()
 
     targetxpt,targetypt=m(targetlon,targetlat)
     m.scatter(targetxpt,targetypt,marker='+',s=50,c='red',linewidth=1.5) 
@@ -149,8 +140,8 @@ instrument='atms_npp'
 BorA='anl'
 #BorA='ges'
 
-#makeplots=True
-makeplots=False
+makeplots=True
+#makeplots=False
 
 # get the gribxtremes points
 
@@ -182,16 +173,24 @@ print targetamps
 
 
 diagfiledir='/data/users/aeichmann/COATsvn/radstat/'
-diagfile = diagfiledir + 'diag_'+instrument+'_'+BorA+'.'+date
-#diagfile ='/data/users/aeichmann/COATsvn/radstat/diag_atms_npp_anl.' + date
-diag_rad = read_diag.diag_rad(diagfile,endian='big')
+diaganlfile = diagfiledir + 'diag_'+instrument+'_anl.'+date
+diagradanl = read_diag.diag_rad(diaganlfile,endian='big')
+
+diaggesfile = diagfiledir + 'diag_'+instrument+'_ges.'+date
+diagradges = read_diag.diag_rad(diaggesfile,endian='big')
 
 
-print 'total number of obs = ',diag_rad.nobs
-diag_rad.read_obs()
+print 'total number anl obs = ',diagradanl.nobs
+diagradanl.read_obs()
 # print o-f stats for one channel
 
-channels= list(set(diag_rad.channel))
+print 'total number ges obs = ',diagradges.nobs
+diagradges.read_obs()
+# print o-f stats for one channel
+
+# assumes channels are same for both anl and ges
+channels= list(set(diagradanl.channel))
+
 #channels=[1,2,3]
 
 
@@ -216,61 +215,97 @@ targetxpt,targetypt=m(targetlons,targetlats)
 
 tablelines=[[] for x in xrange(numgepoints)]
 
-for ichan in channels:
-#for ichan in [6]:
+#for ichan in channels:
+for ichan in [6,7]:
 
     # get indices of all matching channels
-    idxall = diag_rad.channel == ichan
+    idxallanl = diagradanl.channel == ichan
+    idxallges = diagradges.channel == ichan
     # how many obs total
-    nobsall = idxall.sum()
+    nobsallanl = idxallanl.sum()
+    nobsallges = idxallges.sum()
     # get indices of used obs with matching channels
-    idx = np.logical_and(np.logical_and(diag_rad.channel == ichan, diag_rad.used == 1), diag_rad.oberr < 1.e9)
-    nobs = idx.sum()
+
+    # oberr varies between anl and ges, so eliminated for the moment to make them match 
+
+#    idxanl = np.logical_and(np.logical_and(diagradanl.channel == ichan, diagradanl.used == 1), diagradanl.oberr < 1.e9)
+#    idxanl = np.logical_and(diagradanl.channel == ichan, diagradanl.used == 1)
+    idxanl = np.logical_and(np.logical_and(np.logical_and(diagradanl.channel == ichan, \
+            diagradanl.used == 1), diagradanl.oberr < 1.e9), diagradges.oberr < 1.e9)
+
+    nobsanl = idxanl.sum()
+#    idxges = np.logical_and(np.logical_and(diagradges.channel == ichan, diagradges.used == 1), diagradges.oberr < 1.e9)
+#    idxges = np.logical_and(diagradges.channel == ichan, diagradges.used == 1)
+    idxges = np.logical_and(np.logical_and(np.logical_and(diagradges.channel == ichan, \
+            diagradges.used == 1), diagradges.oberr < 1.e9), diagradanl.oberr < 1.e9)
+    nobsges = idxges.sum()
+
+    if nobsges != nobsanl:
+        print 'nobsges = ' + str(nobsges) + ', nobsanl = ' + str(nobsanl)
+        sys.exit()
+
     # skip if no obs used
-    if nobs == 0: continue 
-    fitsq = ((diag_rad.hx[idx]-diag_rad.obs[idx])**2).mean()
+    if nobsanl == 0: continue 
+    fitsq = ((diagradanl.hx[idxanl]-diagradanl.obs[idxanl])**2).mean()
 
-    print diag_rad.obs[6],diag_rad.hx[6],diag_rad.biascorr[6],diag_rad.biaspred[1:,6].sum()
-    print nobs,'obs used for channel',ichan,'out of',nobsall,'rms o-f', np.sqrt(fitsq)
+    print diagradanl.obs[6],diagradanl.hx[6],diagradanl.biascorr[6],diagradanl.biaspred[1:,6].sum()
+    print nobsanl,'anl: obs used for channel',ichan,'out of',nobsallanl,'rms o-f', np.sqrt(fitsq)
 
-    obslats=diag_rad.lat[idx]
-    obslons=diag_rad.lon[idx]
+    print diagradges.obs[6],diagradges.hx[6],diagradges.biascorr[6],diagradges.biaspred[1:,6].sum()
+    print nobsges,'ges: obs used for channel',ichan,'out of',nobsallges,'rms o-f', np.sqrt(fitsq)
+
+    obslats=diagradanl.lat[idxanl]
+    obslons=diagradanl.lon[idxanl]
+
+    if any(obslats != diagradges.lat[idxges]) or any(obslons != diagradges.lon[idxges]):
+        print 'skronk'
+        sys.exit()
+
     for i,val in enumerate(obslons):
         if val > 180.0  : obslons[i] = val - 360.0
     obsxpt,obsypt=m(obslons,obslats)
 
+
     
-    obs=diag_rad.obs[idx]
-    if BorA== 'anl' :
-    	titlestr = 'Tb ana ' + date + ' ' + instrument + ' Ch ' + str(ichan)
-	outfilename=date+'/'+instrument+'_ana_obs_Ch%02d' % ichan + '_' + date + '.jpg'
-    else:
-    	titlestr = 'Tb ges ' + date + ' ' + instrument + ' Ch ' + str(ichan)
-	outfilename=date+'/'+instrument+'_ges_obs_Ch%02d' % ichan + '_' + date + '.jpg'
-    vmin=min(obs)
-    vmax=max(obs)
+    field=diagradanl.obs[idxanl]
+    titlestr = 'Tb ana ' + date + ' ' + instrument + ' Ch ' + str(ichan)
+    outfilename=date+'/'+instrument+'_ana_obs_Ch%02d' % ichan + '_' + date + '.jpg'
+    vmin=min(field)
+    vmax=max(field)
     if makeplots:
     	print 'generating ' + outfilename 
-	plotglobal(m,obs,obsxpt,obsypt,titlestr,vmin,vmax,targetvars,targetamps,targetxpt,targetypt,outfilename)
+	plotglobal(m,field,obsxpt,obsypt,titlestr,vmin,vmax,targetvars,targetamps,targetxpt,targetypt,outfilename)
 
-    omb_bc = diag_rad.obs[idx] - diag_rad.hx[idx]
-    if BorA== 'anl' :
-	titlestr = 'O-A (w/ bc) ' + date + ' ' + instrument + ' Ch ' + str(ichan)
-    	outfilename=date+'/'+instrument+'_ana_OmA_Ch%02d' % ichan + '_' + date + '.jpg'
-    else:
-	titlestr = 'O-B (w/ bc) ' + date + ' ' + instrument + ' Ch ' + str(ichan)
-    	outfilename=date+'/'+instrument+ '_ges_OmB_Ch%02d' % ichan + '_' + date + '.jpg'
+    field=diagradges.obs[idxges]
+    titlestr = 'Tb ges ' + date + ' ' + instrument + ' Ch ' + str(ichan)
+    outfilename=date+'/'+instrument+'_ges_obs_Ch%02d' % ichan + '_' + date + '.jpg'
+    vmin=min(field)
+    vmax=max(field)
+    if makeplots:
+    	print 'generating ' + outfilename 
+	plotglobal(m,field,obsxpt,obsypt,titlestr,vmin,vmax,targetvars,targetamps,targetxpt,targetypt,outfilename)
 
+
+    oma_bc = diagradanl.obs[idxanl] - diagradanl.hx[idxanl]
+    titlestr = 'O-A (w/ bc) ' + date + ' ' + instrument + ' Ch ' + str(ichan)
+    outfilename=date+'/'+instrument+'_ana_OmA_Ch%02d' % ichan + '_' + date + '.jpg'
+
+    vmin=min(oma_bc)
+    vmax=max(oma_bc)
+    if makeplots:
+    	print 'generating ' + outfilename 
+    	plotglobal(m,oma_bc,obsxpt,obsypt,titlestr,vmin,vmax,targetvars,targetamps,targetxpt,targetypt,outfilename)
+
+
+    omb_bc = diagradanl.obs[idxanl] - diagradanl.hx[idxanl]
+    titlestr = 'O-B (w/ bc) ' + date + ' ' + instrument + ' Ch ' + str(ichan)
+    outfilename=date+'/'+instrument+ '_ges_OmB_Ch%02d' % ichan + '_' + date + '.jpg'
     vmin=min(omb_bc)
     vmax=max(omb_bc)
     if makeplots:
     	print 'generating ' + outfilename 
     	plotglobal(m,omb_bc,obsxpt,obsypt,titlestr,vmin,vmax,targetvars,targetamps,targetxpt,targetypt,outfilename)
 
-#    hx = diag_rad.hx[idx]
-#    titlestr = 'hx (w/ bc) ' + date + ' ' + instrument + ' Ch ' + str(ichan)
-#    outfilename=date+'/'+instrument+'_hx_Ch%02d' % ichan + '_' + date + '.jpg'
-#    plotglobal(m,hx,obsxpt,obsypt,titlestr,targetvars,targetamps,targetxpt,targetypt,outfilename)
 
     for i,val in enumerate(targetvars):
 
@@ -292,21 +327,13 @@ for ichan in channels:
         if maxlat > 90.0: maxlat = 90.0
         if minlat < -90.0: minlat = -90.0
 
-        newlons=diag_rad.lon[idx]
-        newlats=diag_rad.lat[idx]
-        newobs=diag_rad.obs[idx]
+        newlons=diagradanl.lon[idxanl]
+        newlats=diagradanl.lat[idxanl]
+        newobs=diagradanl.obs[idxanl]
 
         for j,lontmp in enumerate(newlons):
             if lontmp > 180.0  : newlons[j] = lontmp - 360.0
 
-#        targetidx = np.logical_and(minlat <= diag_rad.lats[idx] <= maxlat, minlon <= diag_rad.lons[idx] <= maxlon )
-        targetidx = np.logical_and(  \
-                np.logical_and( newlats >= minlat , newlats <= maxlat ), \
-                np.logical_and( newlons >= minlon , newlons <= maxlon ))
-
-#        obs=newobs[targetidx]
-#        obslats=newlats[targetidx]
-#        obslons=newlons[targetidx]
         obs=newobs
         obslats=newlats
         obslons=newlons
@@ -328,9 +355,6 @@ for ichan in channels:
                 plottarget(obs,obslats,obslons,maxlat,minlat,maxlon,minlon,titlestr,vmin,vmax,targetvars[i], \
                         targetamps[i],targetlon,targetlat,outfilename)
 
-#	    omb_bc_tgt = omb_bc[targetidx]
-	    omb_bc_tgt = omb_bc
-
             if BorA== 'anl' :
 	        titlestr = 'O-A (w/ bc) ' + date + ' ' + instrument + ' Ch ' + str(ichan) + ' ' + val
                 outfilenamebase=date+'/'+instrument+'_ana_OmA_Ch%02d' % ichan + '_' + date + '_' + \
@@ -348,19 +372,19 @@ for ichan in channels:
             tableline = tableline + outfilename
             if makeplots:
                 print 'generating ' + outfilename 
-                plottarget(omb_bc_tgt,obslats,obslons,maxlat,minlat,maxlon,minlon,titlestr,vmin,vmax,targetvars[i], \
+                plottarget(omb_bc,obslats,obslons,maxlat,minlat,maxlon,minlon,titlestr,vmin,vmax,targetvars[i], \
                     targetamps[i],targetlon,targetlat,outfilename)
 
             tableline = tableline + '"></td><td><img src="'
 
-            vmin=min(omb_bc_tgt)
-	    vmax=max(omb_bc_tgt)
+            vmin=min(omb_bc)
+	    vmax=max(omb_bc)
 #	    outfilename = outfilenamebase + '_rel.tiff'
 	    outfilename = outfilenamebase + '_rel.jpg'
             tableline = tableline + outfilename
             if makeplots:
                 print 'generating ' + outfilename 
-                plottarget(omb_bc_tgt,obslats,obslons,maxlat,minlat,maxlon,minlon,titlestr,vmin,vmax,targetvars[i], \
+                plottarget(omb_bc,obslats,obslons,maxlat,minlat,maxlon,minlon,titlestr,vmin,vmax,targetvars[i], \
                     targetamps[i],targetlon,targetlat,outfilename)
 
             tableline = tableline +  '"></td></tr>\n'
